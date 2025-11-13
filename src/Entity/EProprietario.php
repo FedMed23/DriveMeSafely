4bc71420549567981837246993e27e87c82f5743
martinacamasso03 <118466085+martinacamasso03@users.noreply.github.com>
2025-11-13 12:27:58 +0100

    Update EProprietario.php

--- src/Entity/EProprietario.php
+++ src/Entity/EProprietario.php
@@ -1,11 +1,12 @@
 <?php
+namespace Entity;
 use Doctrine\ORM\Mapping as ORM;
 /**
  * La classe EProprietario estende EUtenteRegistrato.
  * E' l'utente con il massimo livello di accesso amministrativo,
  * Non ha attributi di dominio.
  * @ORM\Entity
- * @ORM\Table(name="Proprietario")
+ * @ORM\Table(name="proprietario")
  */
 class EProprietario extends EUtenteRegistrato {
     
