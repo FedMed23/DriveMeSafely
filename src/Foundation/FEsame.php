ddba42ee0611d870087b60383336c388ce74c9f2
martinacamasso03 <118466085+martinacamasso03@users.noreply.github.com>
2026-03-27 12:41:52 +0100

    Create FEsame.php

---
+++ src/Foundation/FEsame.php
@@ -0,0 +1,82 @@
+<?php
+// src/Foundation/FEsame.php
+
+namespace DriveMeSafely\src\Foundation;
+
+use DriveMeSafely\src\Entity\EEsame;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FEsame
+{
+    private EntityManagerInterface $em;
+
+    // Costruttore: collega Doctrine al database
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    // ---------------------- SALVATAGGIO ----------------------
+    // Salva un nuovo esame
+    public function save(EEsame $esame): void
+    {
+        $this->em->persist($esame);
+        $this->em->flush();
+    }
+
+    // ---------------------- LETTURA ----------------------
+    // Recupera un esame tramite ID
+    public function getEsameById(int $id): ?EEsame
+    {
+        return $this->em->getRepository(EEsame::class)->find($id);
+    }
+
+    // Recupera tutti gli esami
+    public function getAllEsami(): array
+    {
+        return $this->em->getRepository(EEsame::class)->findAll();
+    }
+
+    // ---------------------- AGGIORNAMENTO ----------------------
+    public function update(EEsame $esame): void
+    {
+        $this->em->flush();
+    }
+
+    // ---------------------- ELIMINAZIONE ----------------------
+    public function delete(EEsame $esame): void
+    {
+        $this->em->remove($esame);
+        $this->em->flush();
+    }
+
+    // ---------------------- METODI PERSONALIZZATI ----------------------
+
+    // Trova esami per tipologia (teorico/pratico)
+    public function getEsamiByTipologia(string $tipologia): array
+    {
+        return $this->em->getRepository(EEsame::class)->findBy([
+            'tipologia' => $tipologia
+        ]);
+    }
+
+    // Trova esami in una certa data
+    public function getEsamiByData(\DateTimeImmutable $data): array
+    {
+        return $this->em->getRepository(EEsame::class)->findBy([
+            'dataEs' => $data
+        ]);
+    }
+
+    // Trova esami futuri (molto utile)
+    public function getEsamiFuturi(): array
+    {
+        return $this->em->getRepository(EEsame::class)
+            ->createQueryBuilder('e')
+            ->where('e.dataEs > :oggi')
+            ->setParameter('oggi', new \DateTimeImmutable())
+            ->getQuery()
+            ->getResult();
+    }
+}
+?>
