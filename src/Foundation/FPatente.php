71eb40acb257724267b246a4c94c5dcc29afa03b
martinacamasso03 <118466085+martinacamasso03@users.noreply.github.com>
2026-03-27 12:49:33 +0100

    Create FPatente.php

---
+++ src/Foundation/FPatente.php
@@ -0,0 +1,62 @@
+<?php
+// src/Foundation/FPatente.php
+
+namespace DriveMeSafely\src\Foundation;
+
+use DriveMeSafely\src\Entity\EPatente;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FPatente
+{
+    private EntityManagerInterface $em;
+
+    // Costruttore
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    // ---------------------- SALVATAGGIO ----------------------
+    public function save(EPatente $patente): void
+    {
+        $this->em->persist($patente);
+        $this->em->flush();
+    }
+
+    // ---------------------- LETTURA ----------------------
+    // Recupera per ID
+    public function getPatenteById(int $id): ?EPatente
+    {
+        return $this->em->getRepository(EPatente::class)->find($id);
+    }
+
+    // Recupera tutte le patenti
+    public function getAllPatenti(): array
+    {
+        return $this->em->getRepository(EPatente::class)->findAll();
+    }
+
+    // ---------------------- AGGIORNAMENTO ----------------------
+    public function update(EPatente $patente): void
+    {
+        $this->em->flush();
+    }
+
+    // ---------------------- ELIMINAZIONE ----------------------
+    public function delete(EPatente $patente): void
+    {
+        $this->em->remove($patente);
+        $this->em->flush();
+    }
+
+    // ---------------------- METODI PERSONALIZZATI ----------------------
+
+    // Trova patente per tipo (A, B, C...)
+    public function getByTipo(string $tipo): ?EPatente
+    {
+        return $this->em->getRepository(EPatente::class)->findOneBy([
+            'tipo' => $tipo
+        ]);
+    }
+}
+?>
