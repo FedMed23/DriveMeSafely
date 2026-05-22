7e26856ba449a612f26089bee9c1efe72e4e9047
FedMed23 <med.federica@gmail.com>
2026-05-22 20:43:16 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,12 +3,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use CamassoMedelago\DriveMeSafely\Controller\CIscrizione;
 use CamassoMedelago\DriveMeSafely\Foundation\FIscritto;
 use CamassoMedelago\DriveMeSafely\Foundation\FPatente;
 use Smarty\Smarty;
+use DateTimeImmutable;
 
 // Inizializza Doctrine (configurazione esterna)
 $entityManager = require __DIR__ . "/../bootstrap.php";
@@ -57,19 +59,19 @@ switch ($page) {
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        $iscritto = $cIscrizione->inserisciDati([
-            'nome' => $_POST['nome'],
-            'cognome' => $_POST['cognome'],
-            'email' => $_POST['email'],
-            'username' => $_POST['username'],
-            'password' => $_POST['password'],
-            'stato' => true,
-            'codiceFiscale' => $_POST['codiceFiscale'],
-            'dataNascita' => $_POST['dataNascita'],
-            'luogoNascita' => $_POST['luogoNascita'],
-            'indirizzo' => $_POST['indirizzo'],
-            'numeroTelefono' => $_POST['numeroTelefono'],
-            'tipoPatente' => null
+        'iscritto' = $cIscrizione->inserisciDati([
+                'nome' => $_POST['nome'],
+                'cognome' => $_POST['cognome'],
+                'email' => $_POST['email'],
+                'username' => $_POST['username'],
+                'password' => $_POST['password'],
+                'stato' => true,
+                'codiceFiscale' => $_POST['codiceFiscale'],
+                'dataNascita' => new DateTimeImmutable($_POST['dataNascita']), 
+                'luogoNascita' => $_POST['luogoNascita'],
+                'indirizzo' => $_POST['indirizzo'],
+                'numeroTelefono' => $_POST['numeroTelefono'],
+                'tipoPatente' => null
         ]);
 
         $cIscrizione->confermaDati($_POST['idPa'], $iscritto);
