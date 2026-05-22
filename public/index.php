0692e017febd86a20b6e28e2eac1e44314c95676
FedMed23 <med.federica@gmail.com>
2026-05-22 19:51:49 +0200

    Remove home.tpl display from index.php
    Remove display of 'home.tpl' template.

--- public/index.php
+++ public/index.php
@@ -19,7 +19,6 @@ $smarty->setTemplateDir(__DIR__ . '/../templates/');
 $smarty->setCompileDir(__DIR__ . '/../templates_c/');
 $smarty->setCacheDir(__DIR__ . '/../cache/');
 $smarty->setConfigDir(__DIR__ . '/../configs/');
-$smarty->display('home.tpl');
 
 // Inizializza le fondazioni
 $fIscritto = new FIscritto($entityManager);
