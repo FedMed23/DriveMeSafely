99b98cc944d17b7b19dff9c4630920968eedc359
FedMed23 <med.federica@gmail.com>
2026-05-22 19:14:24 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,65 +3,74 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use CamassoMedelago\DriveMeSafely\Controller\CIscrizione;
+use CamassoMedelago\DriveMeSafely\Foundation\FIscritto;
+use CamassoMedelago\DriveMeSafely\Foundation\FPatente;
+use Smarty\Smarty;
 
-$smarty = new Smarty\Smarty();
+// Inizializza Doctrine (configurazione esterna)
+$entityManager = require __DIR__ . '/../config/doctrine.php';
 
+// Inizializza Smarty
+$smarty = new Smarty();
 $smarty->setTemplateDir(__DIR__ . '/../templates/');
 $smarty->setCompileDir(__DIR__ . '/../templates_c/');
+$smarty->setCacheDir(__DIR__ . '/../cache/');
+$smarty->setConfigDir(__DIR__ . '/../configs/');
 
+// Inizializza le fondazioni
+$fIscritto = new FIscritto($entityManager);
+$fPatente = new FPatente($entityManager);
+
+// Inizializza il controller
+$cIscrizione = new CIscrizione($fIscritto, $fPatente);
+
+// Leggi il parametro 'page' per il routing
 $page = $_GET['page'] ?? 'home';
 
-switch($page) {
+switch ($page) {
+    case 'iscrizione':
+        // Mostra la lista delle patenti
+        $patenti = $cIscrizione->getPatenti();
+        $smarty->assign('patenti', $patenti);
+        $smarty->display('iscrizione/VPatenti.tpl');
+        break;
 
-            case 'iscrizione':
-            
-                $controller = new CIscrizione($fIscritto, $fPatente);
-            
-                $patenti = $controller->getPatenti();
-            
-                $smarty->assign('patenti', $patenti);
-            
-                $smarty->display('iscrizione/VPatenti.tpl');
-            
-            break;
-            case 'inserisciDati':
-            
-                $idPa = $_POST['idPa'];
-            
-                $smarty->assign('idPa', $idPa);
-            
-                $smarty->display('iscrizione/VInserisciDati.tpl');
-            
-            break;
-            case 'confermaIscrizione':
-            
-                $controller = new CIscrizione($fIscritto, $fPatente);
-            
-                $iscritto = $controller->inserisciDati([
-                    'nome' => $_POST['nome'],
-                    'cognome' => $_POST['cognome'],
-                    'email' => $_POST['email'],
-                    'username' => $_POST['username'],
-                    'password' => $_POST['password'],
-                    'stato' => true,
-                    'codiceFiscale' => $_POST['codiceFiscale'],
-                    'dataNascita' => $_POST['dataNascita'],
-                    'luogoNascita' => $_POST['luogoNascita'],
-                    'indirizzo' => $_POST['indirizzo'],
-                    'numeroTelefono' => $_POST['numeroTelefono'],
-                    'tipoPatente' => null
-                ]);
-            
-                $controller->confermaDati(
-                    $_POST['idPa'],
-                    $iscritto
-                );
-            
-                $smarty->assign('iscritto', $iscritto);
-            
-                $smarty->display(
-                    'iscrizione/VConfermaIscrizione.tpl'
-                );
-            
-            break;
+    case 'inserisciDati':
+        // Mostra il form per inserire i dati
+        $idPa = $_POST['idPa'];
+        $smarty->assign('idPa', $idPa);
+        $smarty->display('iscrizione/VInserisciDati.tpl');
+        break;
+
+    case 'confermaIscrizione':
+        // Salva i dati dell'iscritto e conferma
+        $datiIscritto = [
+            'nome' => $_POST['nome'],
+            'cognome' => $_POST['cognome'],
+            'email' => $_POST['email'],
+            'username' => $_POST['username'],
+            'password' => $_POST['password'],
+            'stato' => true,
+            'codiceFiscale' => $_POST['codiceFiscale'],
+            'dataNascita' => new DateTimeImmutable($_POST['dataNascita']),
+            'luogoNascita' => $_POST['luogoNascita'],
+            'indirizzo' => $_POST['indirizzo'],
+            'numeroTelefono' => $_POST['numeroTelefono'],
+            'tipoPatente' => null
+        ];
+
+        // Inserimento dati e conferma
+        $iscritto = $cIscrizione->inserisciDati($datiIscritto);
+        $cIscrizione->confermaDati($_POST['idPa'], $iscritto);
+
+        // Passa l'iscritto alla view di conferma
+        $smarty->assign('iscritto', $iscritto);
+        $smarty->display('iscrizione/VConfermaIscrizione.tpl');
+        break;
+
+    case 'home':
+    default:
+        // Pagina home generica
+        $smarty->display('home.tpl');
+        break;
 }
