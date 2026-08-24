dcab771258fe872f97fd42e8c29ce239777e7df6
FedMed23 <med.federica@gmail.com>
2026-08-24 17:03:32 +0200

    Update CHome.php

--- src/Controller/CHome.php
+++ src/Controller/CHome.php
@@ -1,19 +1,36 @@
 <?php
 namespace CamassoMedelago\DriveMeSafely\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
 use CamassoMedelago\DriveMeSafely\View\VHome;
+
 class CHome
 {
     private VHome $view;
-    public function __construct(VHome $view)
+
+    /**
+     * Il costruttore accetta l'EntityManagerInterface per allineamento con il Front Controller,
+     * e inizializza la View in totale autonomia.
+     */
+    public function __construct(EntityManagerInterface $em)
     {
-        $this->view = $view;
+        // Anche se non usiamo $em qui dentro, serve riceverlo per non far crashare il Front Controller!
+        $this->view = new VHome();
     }
+
+    /**
+     * Punto di ingresso principale gestito dal Front Controller (rotta /home)
+     */
     public function home(): void
     {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
+
+        // Recupera l'oggetto allievo dalla sessione se presente
         $utenteLoggato = $_SESSION['utenteLoggato'] ?? null;
+
+        // Invia i dati alla View per il rendering su Smarty
         $this->view->showHome($utenteLoggato);
     }
 }
