06912cc00a30a98b0c3180c938ef8bc2c1272ea2
FedMed23 <med.federica@gmail.com>
2026-08-24 12:24:59 +0200

    Update Smarty require path in index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../StartSmarty.php';
+require_once __DIR__ . '/../Smarty/StartSmarty.php';
 
 use CamassoMedelago\DriveMeSafely\Controller\CFrontController;
 
