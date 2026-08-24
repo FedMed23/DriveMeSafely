52568a7a585d37a2a86477362698199fa97e4e19
FedMed23 <med.federica@gmail.com>
2026-08-24 12:12:11 +0200

    Update require paths in index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once 'StartSmarty.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../StartSmarty.php';
 
 if (Installation::verificaInstallazione()) {
     $fcontroller = new CFrontController();
