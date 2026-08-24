4cdd451bae472ee0104caad389d1c620e64338f3
FedMed23 <med.federica@gmail.com>
2026-08-24 17:39:43 +0200

    Update StartSmarty.php

--- Smarty/StartSmarty.php
+++ Smarty/StartSmarty.php
@@ -14,7 +14,13 @@ class StartSmarty
         $smarty->setCompileDir(__DIR__ . '/../templates_c/');
         $smarty->setCacheDir(__DIR__ . '/../cache/');
         $smarty->setConfigDir(__DIR__ . '/../configs/');
-
+        
+        // Calcola dinamicamente il context path (es: /DriveMeSafely/public)
+        $contextPath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+        
+        // Assegna l'array 'request' per far funzionare i tuoi template esistenti!
+        $smarty->assign('request', ['contextPath' => $contextPath ]);
+        
         return $smarty;
     }
 }
