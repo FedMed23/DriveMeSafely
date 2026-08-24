70e8b91b37cc7be2d2b14bce4bd9f0acbd2ef328
FedMed23 <med.federica@gmail.com>
2026-08-24 12:21:59 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../Smarty/StartSmarty.php';
+require_once __DIR__ . '/../StartSmarty.php';
 
-if (Installation::verificaInstallazione()) {
-    $fcontroller = new CFrontController();
-    $fcontroller->run($_SERVER['REQUEST_URI']);
-} else {
-    Installation::begin();
-}
+use CamassoMedelago\DriveMeSafely\Controller\CFrontController;
+
+$fcontroller = new CFrontController();
+$fcontroller->run($_SERVER['REQUEST_URI']);
