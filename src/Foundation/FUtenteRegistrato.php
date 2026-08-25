99edf3db42aa8178156fa78358a38b52c4501e2d
martinacamasso03 <118466085+martinacamasso03@users.noreply.github.com>
2026-08-25 13:14:00 +0200

    Update FUtenteRegistrato.php

--- src/Foundation/FUtenteRegistrato.php
+++ src/Foundation/FUtenteRegistrato.php
@@ -14,30 +14,69 @@ class FUtente
         $this->em = $em;
     }
 
+    // ------------------- SAVE -------------------
+
     public function save(EUtenteRegistrato $utente): void
     {
         $this->em->persist($utente);
         $this->em->flush();
     }
 
+    // ------------------- GET BY ID -------------------
+
     public function getById(int $id): ?EUtenteRegistrato
     {
-        return $this->em->getRepository(EUtenteRegistrato::class)->find($id);
+        return $this->em
+            ->getRepository(EUtenteRegistrato::class)
+            ->find($id);
     }
+
+    // ------------------- GET BY EMAIL -------------------
 
     public function getByEmail(string $email): ?EUtenteRegistrato
     {
-        return $this->em->getRepository(EUtenteRegistrato::class)->findOneBy([
-            'email' => $email
-        ]);
+        return $this->em
+            ->getRepository(EUtenteRegistrato::class)
+            ->findOneBy([
+                'email' => $email
+            ]);
     }
+
+    // ------------------- GET BY USERNAME -------------------
 
     public function getByUsername(string $username): ?EUtenteRegistrato
     {
-        return $this->em->getRepository(EUtenteRegistrato::class)->findOneBy([
-            'username' => $username
-        ]);
+        return $this->em
+            ->getRepository(EUtenteRegistrato::class)
+            ->findOneBy([
+                'username' => $username
+            ]);
     }
+
+    // ------------------- GET ALL -------------------
+
+    /**
+     * Restituisce tutti gli utenti registrati ordinati
+     * per cognome e successivamente per nome.
+     *
+     * Equivalente a:
+     * SELECT u FROM UtenteRegistrato u
+     * ORDER BY u.cognome, u.nome
+     *
+     * @return EUtenteRegistrato[]
+     */
+    public function findAll(): array
+    {
+        return $this->em
+            ->getRepository(EUtenteRegistrato::class)
+            ->createQueryBuilder('u')
+            ->orderBy('u.cognome', 'ASC')
+            ->addOrderBy('u.nome', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    // ------------------- DELETE -------------------
 
     public function delete(EUtenteRegistrato $utente): void
     {
