8edb1f7157c3e9268854291b3f1a8593bca5a890
martinacamasso03 <118466085+martinacamasso03@users.noreply.github.com>
2026-08-25 12:44:15 +0200

    Update EProprietario.php

--- src/Entity/EProprietario.php
+++ src/Entity/EProprietario.php
@@ -8,18 +8,57 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="proprietario")
  */
-class EProprietario extends EUtenteRegistrato {
-    
-    public function __construct(
+class EProprietario extends EUtenteRegistrato
+{
+    //------------------- COSTRUTTORI -------------------
+
+    /**
+     * Costruttore vuoto obbligatorio per le specifiche JPA/Hibernate.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setStatoAttivato();
+    }
+
+    /**
+     * Costruttore completo della classe EProprietario.
+     * Invoca il costruttore del genitore per inizializzare tutti gli attributi di base.
+     *
+     * @param string $nome Nome del proprietario
+     * @param string $cognome Cognome del proprietario
+     * @param string $username Username dell'account amministratore
+     * @param string $email Email del proprietario
+     * @param string $password Password dell'account amministratore
+     * @param bool $stato Stato dell'account
+     */
+    public function init(
         string $nome,
         string $cognome,
         string $username,
         string $email,
-        string $password
-    ) {
-        // Costruttore del genitore per inizializzare tutti gli attributi base
-        parent::__construct($nome, $cognome, $username, $email, $password);
+        string $password,
+        bool $stato
+    ): void {
+        parent::__construct(
+            $nome,
+            $cognome,
+            $email,
+            $username,
+            $password,
+            $stato
+        );
+
+        $this->setStatoAttivato();
     }
 
+
+    //------------------- TOSTRING -------------------
+
+    public function __toString(): string
+    {
+        return "Proprietario [" . parent::__toString() . "]";
+    }
 }
+
 ?>
